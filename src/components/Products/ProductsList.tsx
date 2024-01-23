871bfc327f8eeffb855ad6ae43cb830014c0d361
AdahMilly <oamildred@gmail.com>
2024-01-23 09:22:44 +0300

    add product columns

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,24 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { productsApi } from "../../api";
 import { Button, Container, DropdownDivider } from "react-bootstrap";
 import ProductCard from "./ProductCard";
+import AddProduct from "./AddProduct";
 
-type Props = {};
 
-const ProductsList = (props: Props) => {
+
+const ProductsList = () => {
+    const [state, setState] = useState({
+        showModal:false
+    })
   useEffect(() => {
     productsApi.getAllProducts();
   }, []);
+
+   const handleOpenModal = (e: any) => {
+     e.preventDefault();
+     setState((prev) => ({
+       ...prev,
+       showModal: !prev.showModal,
+     }));
+   };
+   const closeModal = () => {
+     setState((prev) => ({ ...prev, showModal: false }));
+   };
   return (
     <Container>
+      {/* <AddProduct onHide={closeModal} show={state?.showModal}/> */}
       <div className="d-flex flex-row w-100 justify-content-between align-items-center gap-3">
         <h1>Products</h1>
-        <div className="search-products d-flex flex-row gap-10">
-          <p>Filter</p>
-          <span>
-            <i className="fa-solid fa-filter"></i>
-          </span>
-          <Button variant="primary">Add Product</Button>
+        <div className="search-products d-flex flex-row justify-space-between align-items-center gap-3">
+          <div className="filter-products d-flex flex-row gap-2">
+            <p>Filter</p>
+            <span>
+              <i className="fa-solid fa-filter"></i>
+            </span>
+          </div>
+          <div className="btn-product m-2">
+            <Button variant="primary" onClick={handleOpenModal}>Add Product</Button>
+          </div>
         </div>
       </div>
       <hr />
