18665b6d367fc9994b38f5427f1a43a6274456ea
AdahMilly <oamildred@gmail.com>
2024-01-25 18:31:24 +0300

    boottrap setup

--- src/components/__test__/Signin.spec.ts
+++ src/components/__test__/Signin.spec.ts
@@ -3,5 +3,5 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import SignIn from "../auth/SignIn";
 
 test("logs in user", () => {
-    // render(<SignIn />)
-})
+  // render(<SignIn />)
+});
