863383882faa7b3d7efcc9f0337b01817ae05ea9
AdahMilly <oamildred@gmail.com>
2024-01-28 22:42:09 +0300

    tests

---
+++ src/components/__test__/NarBar.spec.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+// import NarBar from "../NavBar/NarBar";
+
+describe("Navbar component", () => {
+  it("render navbar", () => {
+    // render(<NarBar />)
+  });
+
+  it("false is falsy", () => {
+    expect(false).toBe(false);
+  });
+});
---
+++ src/components/__test__/Signin.spec.tsx
@@ -0,0 +1,5 @@
+describe("SignIn Component", () => {
+  it("renders Sign in with Google", () => {
+    expect(2).toBe(2);
+  });
+});
